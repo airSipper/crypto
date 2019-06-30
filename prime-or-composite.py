854eb432b3cc0d77435713b6788c3741ee74681f
airSipper <luke543@gmail.com>
2019-06-30 12:18:37 +1000

    Create prime-or-composite.py

---
+++ prime-or-composite.py
@@ -0,0 +1,17 @@
+# Prime or composite
+
+def comp_or_prime(lower, upper):
+	""" show if numbers in a range are prime or composite """
+
+	for num in range(lower,upper + 1):
+	   # prime numbers are greater than 1
+		if num > 1:
+			for i in range(2,num):
+				if (num % i) == 0:
+					print("{} : Composite".format(num))
+					break
+			else:
+				print("{} : Prime !".format(num)) 
+
+## test
+#comp_or_prime(10, 100)
